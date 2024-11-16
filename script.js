ed42c18e62c157f58ffc9327ddf4be9de1de2707
Kaushal Kumar <Kaushal.kr.kr@gmail.com>
2024-11-16 21:04:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your JS code here. If required.
+//your JS code here. If required
+const button = document.querySelector('input[type="button"]')
+
+button.addEventListener('click', () => {
+    const colorSelect = document.getElementById('colorSelect');
+    const selectedIndex = colorSelect.selectedIndex; 
+
+    if (selectedIndex >= 0) {
+        colorSelect.options[selectedIndex].remove(); 
+    }
+});
